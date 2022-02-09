60c7663e06c0355b7ae2bb4d5bbca12c784be694
Dimple Motwani <58204284+DimpleM98@users.noreply.github.com>
2022-02-09 14:24:54 +0530

    Update Practice.java
    added more changes

--- src/main/java/org/example/Practice.java
+++ src/main/java/org/example/Practice.java
@@ -3,6 +3,7 @@ package org.example;
 public class Practice {
     public static void main(String[] args) {
         System.out.println("Hello from practice");
+        System.out.prinltn("hey there, I m from DimpleM98")l
         //practising git
     }
 }
