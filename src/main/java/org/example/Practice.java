4d1591db3eeb3f86b3b734251f599d9897472468
Dimple Motwani <58204284+DimpleM98@users.noreply.github.com>
2022-02-09 12:43:49 +0530

    Update Practice.java

--- src/main/java/org/example/Practice.java
+++ src/main/java/org/example/Practice.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Practice {
     public static void main(String[] args) {
-        System.out.println("Hello from practice");
-        //practising git
+        System.out.println("Hello from branch2");
+        //checkin from DimpleM98
     }
 }
