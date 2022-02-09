4b3035f33e9a5ca1329ef62b38a90821d656a47a
Dim98 <59723829+Dim98@users.noreply.github.com>
2022-02-09 12:54:14 +0530

    Update Practice.java

--- src/main/java/org/example/Practice.java
+++ src/main/java/org/example/Practice.java
@@ -4,5 +4,6 @@ public class Practice {
     public static void main(String[] args) {
         System.out.println("Hello from branch2");
         //checkin from DimpleM98
+        //all done with the code.
     }
 }
